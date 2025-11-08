71d85e24b98d82c0b795c98d7e8ec7b952cbb164
DanielKriegerS <danielschifelbain@gmail.com>
2025-11-08 18:25:39 -0300

    Feat: implemented get all endpoint

--- src/main/java/com/danielks/ProductAPI/controllers/ProductController.java
+++ src/main/java/com/danielks/ProductAPI/controllers/ProductController.java
@@ -1,8 +1,14 @@
 package com.danielks.ProductAPI.controllers;
 
+import com.danielks.ProductAPI.entities.Product;
 import com.danielks.ProductAPI.services.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -13,5 +19,10 @@ public class ProductController {
 
     public ProductController(ProductService service) {
         this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAll() {
+        return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
     }
 }
