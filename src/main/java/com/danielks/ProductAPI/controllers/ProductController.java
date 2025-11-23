a1d6894c3c5794351d4788dc303a8053d1544d65
DanielKriegerS <danielschifelbain@gmail.com>
2025-11-23 19:17:44 -0300

    Feat: implemented CRUD methods and endpoints

--- src/main/java/com/danielks/ProductAPI/controllers/ProductController.java
+++ src/main/java/com/danielks/ProductAPI/controllers/ProductController.java
@@ -4,11 +4,10 @@ import com.danielks.ProductAPI.entities.Product;
 import com.danielks.ProductAPI.services.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
@@ -24,5 +23,26 @@ public class ProductController {
     @GetMapping
     public ResponseEntity<List<Product>> getAll() {
         return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getById(@PathVariable UUID id) {
+        return new ResponseEntity<>(service.geById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<UUID> createProduct(@RequestBody Product product) {
+        return new ResponseEntity<>(service.createProduct(product), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@RequestBody Product product, @PathVariable UUID id) {
+        return new ResponseEntity<>(service.updateProduct(product, id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable UUID id) {
+        service.deleteProduct(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/danielks/ProductAPI/services/ProductService.java
+++ src/main/java/com/danielks/ProductAPI/services/ProductService.java
@@ -5,6 +5,7 @@ import com.danielks.ProductAPI.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ProductService {
@@ -17,5 +18,32 @@ public class ProductService {
 
     public List<Product> getAll() {
         return repository.findAll().stream().toList();
+    }
+
+    public Product geById(UUID id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product with id "+ id +" not found"));
+    }
+
+    public UUID createProduct(Product product) {
+        product.validate();
+        Product savedProduct = repository.save(product);
+        return savedProduct.getId();
+    }
+
+    public Product updateProduct(Product product, UUID id) {
+        Product oldProduct = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product with id "+ id +" not found"));
+
+        oldProduct.setName(product.getName());
+        oldProduct.setProductValue(product.getProductValue());
+        return repository.save(oldProduct);
+    }
+
+    public void deleteProduct(UUID id) {
+        repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product with id "+ id +" not found"));
+
+        repository.deleteById(id);
     }
 }
